c93857409b0c3173f8bba223544a68d7cfc547c6
idongessien <idong.essien@comcast.net>
2020-11-01 01:53:28 -0400

    added top bar, under construction text, footer

---
+++ src/Container.jsx
@@ -0,0 +1,9 @@
+export default function Container() {
+    return(
+        <div className="main-container">
+            <p className="construction">Hello 👋</p>
+            <p className="construction">🚧 Please, excuse the look</p>
+            <p className="construction">I am under construction 👷</p>
+        </div>
+    );
+}
