d674e62402e9e61772e8fbc74d6615ea31a6a1ab
idongessien <idong.essien@comcast.net>
2020-11-08 21:30:24 -0500

    Added and styled nav/menu and fixed toggle functionality

--- src/js/MainNav.jsx
+++ src/js/MainNav.jsx
@@ -1,26 +1,12 @@
-import styled from 'styled-components';
-
-const MainNavStyle = styled.nav`
-height:45vh;
-position:absolute;
-right:25px;
-background:skyblue;
-width:240px;
-border-radius:18px;
-`
-const ulStyle = {
-'border':'1px dashed yellow',
-'height':'100%',
-}
-
 export default function MainNav(){
     return(
-        <MainNavStyle>
-            <ul style={ulStyle}>
+        <nav>
+            <ul>
                 <li>About</li>
                 <li>Portfolio</li>
+                <li>Resume</li>
                 <li>Contact</li>
             </ul>
-        </MainNavStyle>
+        </nav>
     );
 }
