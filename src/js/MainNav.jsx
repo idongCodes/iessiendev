de94ae4a4aebb924723ca1d0ec83cee52367550b
idongessien <idong.essien@comcast.net>
2020-11-08 19:05:10 -0500

    added navigation

--- src/js/MainNav.jsx
+++ src/js/MainNav.jsx
@@ -1,11 +1,26 @@
+import styled from 'styled-components';
+
+const MainNavStyle = styled.nav`
+height:45vh;
+position:absolute;
+right:25px;
+background:skyblue;
+width:240px;
+border-radius:18px;
+`
+const ulStyle = {
+'border':'1px dashed yellow',
+'height':'100%',
+}
+
 export default function MainNav(){
     return(
-        <nav>
-            <ul>
+        <MainNavStyle>
+            <ul style={ulStyle}>
                 <li>About</li>
                 <li>Portfolio</li>
                 <li>Contact</li>
             </ul>
-        </nav>
+        </MainNavStyle>
     );
 }
---
+++ src/js/MenuIcon.jsx
@@ -0,0 +1,36 @@
+import MainNav from './MainNav';
+
+const menu = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+
+// Styles
+const menuContainer = {
+    'position':'absolute',
+    'top':'0',
+    'right':'0'
+}
+
+const ionIcon = {
+    'position':'relative',
+    'zIndex':'99'
+}
+
+const toggleMenu = () => {
+   if (nav.style.display === "block") {
+    nav.style.display = "none";
+   } else {
+       nav.style.display = "block";
+    }
+
+}
+
+const MenuIcon = () => {
+    return (
+        <div style={menuContainer}>
+            <ion-icon id="menu" style={ionIcon} onClick={toggleMenu} name="menu-sharp"></ion-icon>
+            <MainNav />
+        </div>
+    );
+}
+
+export default MenuIcon;
