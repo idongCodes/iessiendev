87004dd47e4a9530553388d8be553d025d2eb858
Idong Essien <idong.essien@comcast.net>
2020-11-24 17:30:10 -0500

    Removed Sidebar animation, sidebar is fixed position, added menu icon animation, moved menu icon to top bar fixed position

---
+++ src/js/MenuIcon.js
@@ -0,0 +1,11 @@
+function MenuIcon() {
+    return (
+        <div className='menu-container'>
+            <div className='menu-top'></div>
+            <div className='menu-mid'></div>
+            <div className='menu-bottom'></div>
+        </div>
+    );
+}
+
+export { MenuIcon };
