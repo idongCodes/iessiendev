50bfe8de0720b7bf910a46eda31beb128e122c58
Idong Essien <idong.essien@comcast.net>
2021-01-02 23:07:20 -0500

    Uploaded necessary directories and files for re-design

--- src/js/functions.js
+++
@@ -1,6 +0,0 @@
-// var myMenu = document.querySelector('.menu-container');
-// var myNav = document.querySelector('.nav');
-
-// myMenu.addEventListener('mouseenter', function() {
-//     myNav.classList.add('menu-slide-open');
-// });
